78148c7018b11fd458a15f0af5a01ce6516c5048
Praveen Prasad <veen.edu@gmail.com>
2016-11-06 22:05:21 +0530

    ~

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,5 +5,5 @@ export ColumnHeader from './Column/ColumnHeader.react';
 export PanelContainer from './Panel/PanelContainer.react';
 export PanelItem from './Panel/PanelItem.react';
 export Container from './Container.react';
-export RootContainer from './RootContainer.react';
+// export RootContainer from './RootContainer.react';
 export ScrollView from './ScrollView.react';
