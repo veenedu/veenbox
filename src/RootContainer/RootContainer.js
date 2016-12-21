3ced518620d99c82590053f5d9e97c8e83bf1ecc
Praveen Prasad <praveenprasad@Praveens-MacBook-Pro.local>
2016-12-21 11:10:45 +0530

    Fix issue in ReactNative Root Container

--- src/RootContainer/RootContainer.js
+++ src/RootContainer/RootContainer.js
@@ -1,1 +1,15 @@
-export  {View as default}  from 'react-native';
+import React, { Component } from 'react';
+import  {View}  from 'react-native';
+
+const Container = ({children,style}) => {
+  let addStyle = {
+  	flex:1
+  }
+
+  return <View 
+  		style={{...addStyle, ...style}}>
+  		{children}
+  </View>
+}
+
+export default Container;
