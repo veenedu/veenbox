0be05f1afba92e4e055dd17ca6937c57bd0131ab
Praveen Prasad <praveenprasad@Praveens-MacBook-Pro.local>
2017-02-10 22:44:36 +0530

    Temporarirly Expose  Old Layout Manager and WindowResize

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import Col from './Col';
 import ScrollView  from './ScrollView'
 import RootContainer  from './RootContainer'
 import {setDefaultDirection,setRootElement} from './configure'
+import LayoutManager from './old/LayoutManager/index.js';
+import {windowResize} from './old/utils/index.r';
 
 
-export {Grid,Row,Col,ScrollView,RootContainer,setDefaultDirection,setRootElement}
+export {Grid,Row,Col,ScrollView,RootContainer
+    ,setDefaultDirection,setRootElement
+    ,LayoutManager,windowResize
+}
